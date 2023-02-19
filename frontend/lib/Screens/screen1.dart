11d85a03be6cf46c7e47549adc6b43eff011cf43
Abhijna Raghavendra <a_raghavendra@ma.iitr.ac.in>
2023-02-19 11:49:50 +0530

    feat: Initial UI of screen1

--- frontend/lib/Screens/screen1.dart
+++ frontend/lib/Screens/screen1.dart
@@ -1,15 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:frontend/UI/search_bar.dart';
 import 'package:frontend/UI/bottom_navbar.dart';
+import 'package:frontend/UI/property_card.dart';
 
 class Screen1 extends StatelessWidget {
   const Screen1({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: SearchInput(),
-      bottomNavigationBar: CustomNavbar(),
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: const <Widget>[
+          SearchInput(),
+          PropertyCard(),
+        ],
+      ),
+      bottomNavigationBar: const CustomNavbar(),
     );
   }
 }
